d1f35bbf35cd5241f5466172e464067782791ae2
Bernardo Afonso <bernardoafonso9734@gmail.com>
2022-01-19 11:39:28 -0300

    Formulario para receber os valores

---
+++ primeiro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página 1</title>
+</head>
+<body>
+<form method="POST" action="segundo.php">
+    <label for="n1">Numero 1</label>
+    <input type="text" name="n1">
+    <label for="n2">Numero 2</label>
+    <input type="text" name="n2">
+    <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
